308fbcc92e55b15c992e3456ff835385484b58f3
sks3 <saketsingh2018@gmail.com>
2018-03-17 02:11:03 -0500

    Pull to refresh

--- twitter_alamofire_demo/TimelineViewController.swift
+++ twitter_alamofire_demo/TimelineViewController.swift
@@ -9,65 +9,86 @@
 import UIKit
 
 class TimelineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  var tweets: [Tweet] = []
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    var tweets: [Tweet] = []
+    // Initialize a UIRefreshControl
+    let refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+    // add refresh control to table view
+    tableView.insertSubview(refreshControl, at: 0)
     
-    @IBOutlet weak var tableView: UITableView!
+    tableView.dataSource = self
+    tableView.delegate = self
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        //tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 80
-        
-        APIManager.shared.getHomeTimeLine { (tweets, error) in
-            if let tweets = tweets {
-                self.tweets = tweets
-                self.tableView.reloadData()
-            } else if let error = error {
-                print("Error getting home timeline: " + error.localizedDescription)
-            }
-        }
+    //tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 80
+    
+    APIManager.shared.getHomeTimeLine { (tweets, error) in
+      if let tweets = tweets {
+        self.tweets = tweets
+        self.tableView.reloadData()
+      } else if let error = error {
+        print("Error getting home timeline: " + error.localizedDescription)
+      }
     }
+  }
+  
+  func refreshControlAction(_ refreshControl: UIRefreshControl) {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tweets.count
+    APIManager.shared.getHomeTimeLine { (tweets, error) in
+      if let tweets = tweets {
+        self.tweets = tweets
+        self.tableView.reloadData()
+      } else if let error = error {
+        print("Error getting home timeline: " + error.localizedDescription)
+      }
     }
+    refreshControl.endRefreshing()
+  }
+  
+  
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tweets.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetCell
+    cell.selectionStyle = .none
+    cell.tweet = tweets[indexPath.row]
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetCell
-        cell.selectionStyle = .none
-        cell.tweet = tweets[indexPath.row]
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    @IBAction func didTapLogout(_ sender: Any) {
-        APIManager.shared.logout()
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  @IBAction func didTapLogout(_ sender: Any) {
+    APIManager.shared.logout()
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
